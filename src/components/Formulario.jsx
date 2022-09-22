5322b17a27dac1f72b9bb109c04861012a3eca16
elDanmo <daniel_mora0407@outlook.com>
2022-09-22 16:16:43 -0500

    Se modifican las rutas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -51,7 +51,7 @@ const Formulario = ({cliente,cargando}) => {
 
             await respuesta.json()
 
-            navigate('/clientes')
+            navigate('/')
 
         } catch (error) {
             console.log(error)
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <div className="md:w-1/4 bg-blue-900 px-5 py-10">
             <h2 className="text-4xl Font-black text-center text-white">CRM – Clientes</h2>
             <nav className="mt-10">
-              <Link to="/clientes" className={`${urlActual === '/clientes' ? 'text-yellow-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}>Clientes</Link>
+              <Link to="/" className={`${urlActual === '/clientes' ? 'text-yellow-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}>Clientes</Link>
               <Link to="/clientes/nuevo" className={`${urlActual === '/clientes/nuevo' ? 'text-yellow-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}>Nuevo cliente</Link>
             </nav>
         </div>
